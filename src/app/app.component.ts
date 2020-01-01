ced55b87d3570f3fa9c81ca87cf7602605a6f601
mgechev <mgechev@gmail.com>
2020-01-01 11:47:01 -0800

    Add scully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {IdleMonitorService} from '@scullyio/ng-lib'; 
+ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { 
+ constructor (private idle: IdleMonitorService) { } 
+
   title = 'podcast';
 }
