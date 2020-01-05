4aceeb71a57895d722981f09b61651dab0c71b7c
mgechev <mgechev@gmail.com>
2020-01-05 13:31:03 -0800

    Implement seek

--- src/app/shared/player.component.ts
+++ src/app/shared/player.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
+const RADIUS = 50;
+
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
@@ -38,13 +40,28 @@ export class PlayerComponent implements AfterViewInit {
     });
   }
 
-  // (x – h)2 + (y – k)2 = r2
   seek(evnt: MouseEvent) {
-    const x = evnt.offsetX;
-    const y = evnt.offsetY;
-    const r = 50;
-    const h = 50;
-    const k = 50;
-    console.log(evnt.offsetX, evnt.offsetY);
+    const ratio = this._calculateAngle(evnt) / 360;
+    const audio = this.audio.nativeElement as HTMLAudioElement;
+    const seekTo = ratio * audio.duration;
+    audio.currentTime = seekTo;
+  }
+
+  private _calculateAngle(evnt: MouseEvent) {
+    const x = (RADIUS * 2) / (this.width / evnt.offsetX);
+    const y = (RADIUS * 2) / (this.width / evnt.offsetY);
+    const slope = (RADIUS - y) / (RADIUS - x);
+    const angle = 180 * (Math.abs(Math.atan(slope)) / Math.PI);
+
+    if (x < RADIUS && y > RADIUS) {
+      return angle;
+    }
+    if (x > RADIUS && y > RADIUS) {
+      return 180 - angle;
+    }
+    if (x > RADIUS && y < RADIUS) {
+      return 180 + angle;
+    }
+    return 180 + (180 - angle);
   }
 }
